c988c6b7202ead97009368d3a520ebded54aa20c
Sergei <kvakazuabr@yandex.ru>
2023-04-30 22:56:00 +0300

    feat: add job keypress buttons

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 import { getEntryField } from './js/entry-field.js';
-import { buttonsEn, getKeyborsField } from './js/keybord.js';
+import { russianLetters, inglishLetters, getKeyboardHTML, getKeyborsField } from './js/keybord.js';
 import './js/input.js'
+let language = 'ru'
+let buttons = language === 'en' ? getKeyboardHTML(inglishLetters) : getKeyboardHTML(russianLetters);
+console.log(getKeyboardHTML);
 
 const keybordSection = `<section class="keybord-section">
   ${getEntryField()}
-  ${getKeyborsField(buttonsEn)}
+  ${getKeyborsField(buttons)}
 </section>`;
 
 document.body.insertAdjacentHTML('afterend', keybordSection);
 
 document.querySelector('.keybord-section__wrap').addEventListener('click', (evt) => {
   if (evt.target.children.length) {
-    document.getElementById('field').value += evt.target.children[0].textContent;
+    console.log(evt.target.children.length);
+    // document.getElementById('field').value += evt.target.children[0].textContent;
   } else {
     document.getElementById('field').value += evt.target.textContent
   }
--- src/js/keybord.js
+++ src/js/keybord.js
@@ -1,25 +1,30 @@
-let russianLetters = [['ё', [1, '!'], [2, '@'], [3, '#'], [4, '$'], [5, '%'], [6, '^'], [7, '&'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ['[', '{'], [']', '}'], ['\\', '|']], ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', [';', ':'], ['\'', '"'], 'Enter'], ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&uarr;', 'Shift'], ['Ctrl', 'Meta', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;']];
+let russianLetters = [['ё', [1, '!'], [2, '"'], [3, '№'], [4, ';'], [5, '%'], [6, ':'], [7, '?'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', ['\\', '|'], 'Del'], ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'], ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ['.', ','], '&uarr;', 'Shift'], ['Ctrl', 'Meta', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;']];
 
-let inglishLetters = [[['`', '~'], [1, '!'], [2, '@'], [3, '#'], [4, '$'], [5, '%'], [6, '^'], [7, '&'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ['[', '{'], [']', '}'], ['\\', '|']], ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', [';', ':'], ['\'', '"'], 'Enter'], ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&uarr;', 'Shift'], ['Ctrl', 'Meta', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;']]
+let inglishLetters = [[['`', '~'], [1, '!'], [2, '@'], [3, '#'], [4, '$'], [5, '%'], [6, '^'], [7, '&'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ['[', '{'], [']', '}'], ['\\', '|'], 'Del'], ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', [';', ':'], ['\'', '"'], 'Enter'], ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', ['/', '?'], '&uarr;', 'Shift'], ['Ctrl', 'Meta', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;']]
 
-const buttonsEn = inglishLetters.map(it => `<div class="keybord-section__row">${it.map(el => {
-  if (Array.isArray(el)) {
-    return `<button class="keybord-section__btn"><span>${el[0]}</span><span class="keybord-section__shift-show">${el[1]}</span></button>`
-  } else {
-    let btn;
-    if (el.length === 1) {
-      btn = `<button data-btn=${el} class="keybord-section__btn">${el.toUpperCase()}</button>`
-    } else if (el === 'Space') {
-      btn = `<button data-btn=${el} class="keybord-section__btn keybord-section__btn--service keybord-section__btn--space">${' '}</button>`
+
+function getKeyboardHTML(array) {
+  return array.map(it => `<div class="keybord-section__row">${it.map(el => {
+    if (Array.isArray(el)) {
+      return `<button class="keybord-section__btn"><span>${el[0]}</span><span class="keybord-section__shift-show">${el[1]}</span></button>`
     } else {
-      btn = `<button data-btn=${el} class="keybord-section__btn keybord-section__btn--service">${el}</button>`
+      let btn;
+      if (el.length === 1) {
+        btn = `<button data-btn=${el} class="keybord-section__btn">${el.toUpperCase()}</button>`
+      } else if (el === 'Space') {
+        btn = `<button data-btn=${el} class="keybord-section__btn keybord-section__btn--service keybord-section__btn--space">${' '}</button>`
+      } else {
+        btn = `<button data-btn=${el} class="keybord-section__btn keybord-section__btn--service">${el}</button>`
+      }
+      return btn;
     }
-    return btn;
-  }
-}).join('')}</div>`).join('\n');
+  }).join('')}</div>`).join('\n');
+  
+}
 
 function getKeyborsField(letters) {
   return `<div class="keybord-section__wrap">${letters}</div>`
 }
 
-export {buttonsEn, getKeyborsField}
+export {russianLetters, inglishLetters, getKeyboardHTML, getKeyborsField}
+''
