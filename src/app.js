47906ceb8bd9fabecb16298530772171207e10d4
Sergei <kvakazuabr@yandex.ru>
2023-05-01 16:56:29 +0300

    feat: add functional

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import { getEntryField } from './js/entry-field.js';
 import { russianLetters, inglishLetters, getKeyboardHTML, getKeyborsField } from './js/keybord.js';
 import './js/input.js'
-let language = 'ru'
+
+
+let language = 'en'
 let buttons = language === 'en' ? getKeyboardHTML(inglishLetters) : getKeyboardHTML(russianLetters);
-console.log(getKeyboardHTML);
+
 
 const keybordSection = `<section class="keybord-section">
   ${getEntryField()}
@@ -12,12 +14,60 @@ const keybordSection = `<section class="keybord-section">
 
 document.body.insertAdjacentHTML('afterend', keybordSection);
 
-document.querySelector('.keybord-section__wrap').addEventListener('click', (evt) => {
-  if (evt.target.children.length) {
-    console.log(evt.target.children.length);
-    // document.getElementById('field').value += evt.target.children[0].textContent;
+function removeBackgroundColor() {
+  [...document.querySelectorAll('.keybord-section__btn')].forEach(el => el.style.backgroundColor = '');
+}
+
+// document.querySelector('.keybord-section__wrap').addEventListener('click', (evt) => {
+//   if (evt.target instanceof HTMLButtonElement || evt.target.parentElement instanceof HTMLButtonElement) {
+//      removeBackgroundColor()
+//      let btn = evt.target
+//      if (evt.target.children.length) {
+//       document.getElementById('field').value += btn.children[0].textContent.toLowerCase();
+//       btn.style.backgroundColor = '#edaeda'
+//     } else {
+//       document.getElementById('field').value += btn.textContent.toLowerCase()
+//       btn.style.backgroundColor = '#edaeda'
+//     }
+//   }
+// })
+
+let events = new Set()
+
+addEventListener("keydown", (evt) => {
+  console.log(evt.key);
+  if (evt.key === ' ') {
+    events.add(evt.code)
+  } else if (evt.key === 'Control') {
+    events.add(evt.code)
+  } else if (evt.key === 'Shift') {
+    events.add(evt.code)
   } else {
-    document.getElementById('field').value += evt.target.textContent
+    events.add(evt.key)
   }
-  
+  removeBackgroundColor()
+  console.log(events[0]);
+});
+
+
+window.addEventListener('keyup', (evt) => {
+  events.forEach((el) => {
+    if (el.length === 1) {
+      el = el.toLowerCase()
+    }
+    let btn = document.getElementById(el);
+    if (btn) {
+      btn.style.backgroundColor = '#edaeda'
+    }
+  })
+  events.clear()
 })
+
+// addEventListener("keydown", function(event) {
+//   if (event.keyCode == 86)
+//     document.body.style.background = "violet";
+// });
+// addEventListener("keyup", function(event) {
+//   if (event.keyCode == 86)
+//     document.body.style.background = "";
+// });
--- src/js/keybord.js
+++ src/js/keybord.js
@@ -1,20 +1,35 @@
-let russianLetters = [['ё', [1, '!'], [2, '"'], [3, '№'], [4, ';'], [5, '%'], [6, ':'], [7, '?'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', ['\\', '|'], 'Del'], ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'], ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ['.', ','], '&uarr;', 'Shift'], ['Ctrl', 'Meta', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;']];
+let russianLetters = [['ё', [1, '!'], [2, '\"'], [3, '№'], [4, ';'], [5, '%'], [6, ':'], [7, '?'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', ['\\', '|'], 'Delete'], ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'], ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ['.', ','], 'ArrowUp', 'Shift'], ['ControlLeft', 'Meta', 'Alt', 'Space', 'Alt', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight']];
 
-let inglishLetters = [[['`', '~'], [1, '!'], [2, '@'], [3, '#'], [4, '$'], [5, '%'], [6, '^'], [7, '&'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ['[', '{'], [']', '}'], ['\\', '|'], 'Del'], ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', [';', ':'], ['\'', '"'], 'Enter'], ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', ['/', '?'], '&uarr;', 'Shift'], ['Ctrl', 'Meta', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;']]
+let inglishLetters = [[['`', '~'], [1, '!'], [2, '@'], [3, '#'], [4, '$'], [5, '%'], [6, '^'], [7, '&'], [8, '*'], [9, '('], [0, ')'], ['-', '_'], ['=', '+'], 'Backspace'], ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ['[', '{'], [']', '}'], ['\\', '|'], 'Delete'], ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', [';', ':'], ['\'', '"'], 'Enter'], ['ShiftLeft', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', ['/', '?'], 'ArrowUp', 'ShiftRight'], ['ControlLeft', 'Meta', 'Alt', 'Space', 'Alt', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight']]
 
 
 function getKeyboardHTML(array) {
   return array.map(it => `<div class="keybord-section__row">${it.map(el => {
     if (Array.isArray(el)) {
-      return `<button class="keybord-section__btn"><span>${el[0]}</span><span class="keybord-section__shift-show">${el[1]}</span></button>`
+      return `<button id=${el[0]} id='${el[1]}' class="keybord-section__btn"><span>${el[0]}</span><span class="keybord-section__shift-show" readonly>${el[1]}</span></button>`
     } else {
       let btn;
       if (el.length === 1) {
-        btn = `<button data-btn=${el} class="keybord-section__btn">${el.toUpperCase()}</button>`
+        btn = `<button id=${el} class="keybord-section__btn">${el.toUpperCase()}</button>`
       } else if (el === 'Space') {
-        btn = `<button data-btn=${el} class="keybord-section__btn keybord-section__btn--service keybord-section__btn--space">${' '}</button>`
-      } else {
-        btn = `<button data-btn=${el} class="keybord-section__btn keybord-section__btn--service">${el}</button>`
+        btn = `<button id=${el} class="keybord-section__btn keybord-section__btn--service keybord-section__btn--space">${' '}</button>`
+      } else if (el === 'ArrowUp') {
+        btn = `<button id=${el} class="keybord-section__btn keybord-section__btn--service">&uarr;</button>`
+      } else if (el === 'ArrowLeft') {
+        btn = `<button id=${el} class="keybord-section__btn keybord-section__btn--service">&larr;</button>`
+      } else if (el === 'ArrowDown') {
+        btn = `<button id=${el} class="keybord-section__btn keybord-section__btn--service">&darr;</button>`
+      } else if (el === 'ArrowRight') {
+        btn = `<button id=${el} class="keybord-section__btn keybord-section__btn--service">&rarr;</button>`
+      } else if (el === 'ControlLeft' || el === 'ControlRight') {
+        btn = `<button id=${el} class="keybord-section__btn keybord-section__btn--service">Ctrl</button>`
+      } else if (el === 'ShiftLeft' || el === 'ShiftRight') {
+        btn = `<button id=${el} class="keybord-section__btn keybord-section__btn--service">Shift</button>`
+      } else if (el === 'Delete') {
+        btn = `<button id=${el} class="keybord-section__btn keybord-section__btn--service">Del</button>`
+      }
+      else {
+        btn = `<button id=${el} class="keybord-section__btn keybord-section__btn--service"">${el}</button>`
       }
       return btn;
     }
